9f404e71817a2873f5abf348e9be86bbc2ae846c
shubham-king <84066395+shubham-king@users.noreply.github.com>
2022-05-08 09:17:14 +0530

    Update __main__.py

--- spr/__main__.py
+++ spr/__main__.py
@@ -48,10 +48,10 @@ async def help_command(_, message: Message):
         return await message.reply("Pm Me For Help", reply_markup=kb)
     kb = ikb(
         {
-            "Help": "bot_commands",
-            "Repo": "https://github.com/TheHamkerCat/SpamProtectionRobot",
-            "Add Me To Your Group": f"https://t.me/{BOT_USERNAME}?startgroup=new",
-            "Support Chat (for now)": "https://t.me/WBBSupport",
+            "Help 🇮🇳": "bot_commands",
+            "📢 Channel": "https://t.me/RoboTez",
+            "⭐ Add Me To Your Group": f"https://t.me/{BOT_USERNAME}?startgroup=new",
+            
         }
     )
     mention = message.from_user.mention
